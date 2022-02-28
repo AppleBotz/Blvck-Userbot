08a06f5452eff1f9843374721c88ee0ed2b333c3
AppleBotz <95849390+AppleBotz@users.noreply.github.com>
2022-02-28 13:17:23 +0700

    Update sosmed.py

--- userbot/modules/sosmed.py
+++ userbot/modules/sosmed.py
@@ -163,16 +163,43 @@ async def _(event):
         await xx.delete()
 
 
-CMD_HELP.update(
-    {
-        "sosmed": f"**Plugin : **`sosmed`\
-        \n\n  •  **Syntax :** `{cmd}sosmed` <link>\
-        \n  •  **Function : **Download Media Dari Pinterest / Tiktok / Instagram.\
-        \n\n  •  **Syntax :** `{cmd}dez` <link>\
-        \n  •  **Function : **Download Lagu Via Deezloader\
-    "
-    }
-)
+@man_cmd(pattern="ttsong(?: |$)(.*)")
+async def _(event):
+    xxnx = event.pattern_match.group(1)
+    if xxnx:
+        d_link = xxnx
+    elif event.is_reply:
+        d_link = await event.get_reply_message()
+    else:
+        return await edit_delete(
+            event,
+            "**Berikan Link Tiktok Pesan atau Reply Link Tiktok Untuk di Download**",
+        )
+    xx = await edit_or_reply(event, "`Video song Sedang Diproses...`")
+    chat = "@ttsongsbot"
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            video = await conv.get_response()
+            text = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.client(UnblockRequest(chat))
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            video = await conv.get_response()
+            text = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        await event.client.send_file(event.chat_id, video)
+        await event.client.delete_messages(
+            conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id, text.id]
+        )
+        await xx.delete()
 
 
 CMD_HELP.update(
